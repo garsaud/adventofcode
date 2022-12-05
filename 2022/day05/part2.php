d5f48e27f66951c873f76be72f03d9db3b6ea90c
Cyril Garsaud <cyril.garsaud@gmail.com>
2022-12-05 23:02:04 +0100

    day5 part2

---
+++ 2022/day05/part2.php
@@ -0,0 +1,28 @@
+<?php
+
+$input = stream_get_contents(STDIN);
+
+[$inputStacks, $inputMoves] = explode("\n\n", $input);
+
+$stacksLines = array_map('str_split', explode("\n", $inputStacks));
+$stacksColumns = array_filter(
+    // transpose lines into columns
+    array_map(null, ...$stacksLines),
+    // remove gaps and brackets
+    fn($line) => preg_match('/\w/', implode($line))
+);
+$stacks = array_map(
+    // remove empty blocks
+    fn($column) => str_split(trim(implode($column))),
+    array_values($stacksColumns)
+);
+
+$moves = array_filter(explode("\n", $inputMoves));
+foreach ($moves as $line) {
+    preg_match('/move (\d+) from (\d+) to (\d+)/', $line, $instructions);
+    [, $amount, $from, $to] = $instructions;
+    $boxes = array_splice($stacks[$from-1], 0, $amount);
+    array_unshift($stacks[$to-1], ...$boxes);
+}
+
+echo implode(array_column($stacks, 0));
