e529819d3be3484834b65bd2e0bf9be470735fc9
Cyril Garsaud <cyril.garsaud@gmail.com>
2023-12-09 00:29:07 +0100

    2023: day8 part1 (refacto)

--- 2023/day08/part1.php
+++ 2023/day08/part1.php
@@ -3,19 +3,19 @@
 $instructions = str_split(trim(fgets(STDIN)));
 fgets(STDIN); // skip empty line
 
-$steps = [];
+$nodes = [];
 while ($line = trim(fgets(STDIN))) {
     preg_match('/(?<S>\w+) = \((?<L>\w+), (?<R>\w+)\)/', $line, $matches);
-    $steps[$matches['S']] = $matches;
+    $nodes[$matches['S']] = $matches;
 }
 
-for (
-    $currentStep = array_key_first($steps), $i = 0, $j = 0;
-    $currentStep != 'ZZZ';
-    $j++, $i++, $i%=count($instructions)
-) {
-    $direction = $instructions[$i];
-    $currentStep = $steps[$currentStep][$direction];
+foreach ($nodes as &$node) {
+    $node['L'] = &$nodes[$node['L']];
+    $node['R'] = &$nodes[$node['R']];
+}
+
+for ($j = 0, $theNode = reset($nodes); $theNode['S'] !== 'ZZZ'; $j++) {
+    $theNode = $theNode[$instructions[$j % count($instructions)]];
 }
 
 echo $j;
