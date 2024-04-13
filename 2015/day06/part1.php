b488d4af8679f04d4655d3cf6adfd3a7fcec9d31
Cyril Garsaud <cyril.garsaud@gmail.com>
2024-04-13 18:30:24 +0200

    2015: day6 slightly optimized

--- 2015/day06/part1.php
+++ 2015/day06/part1.php
@@ -6,23 +6,19 @@ $start = microtime(true);
 $lights = array_fill(0, 1000*1000, 0);
 
 while (($line = fgets(STDIN)) !== false) {
-    preg_match('/(?<todo>[a-z ]+) (?<x1>\d+),(?<y1>\d+) through (?<x2>\d+),(?<y2>\d+)/', $line, $matches);
-    $x1 = (int) $matches['x1'];
-    $x2 = (int) $matches['x2'];
-    $y1 = (int) $matches['y1'];
-    $y2 = (int) $matches['y2'];
+    [$todo, $x1, $y1, $x2, $y2] = sscanf($line, '%8[a-z ] %d,%d through %d,%d');
 
     for ($x = $x1; $x <= $x2; $x++) {
         for ($y = $y1; $y <= $y2; $y++) {
             $index = 1000 * $x + $y;
-            switch ($matches['todo']) {
-                case 'turn on':
+            switch ($todo) {
+                case 'turn on ':
                     $lights[$index] = 1;
                     continue 2;
                 case 'turn off':
                     $lights[$index] = 0;
                     continue 2;
-                case 'toggle':
+                case 'toggle ':
                     $lights[$index] ^= 1;
                     continue 2;
             }
