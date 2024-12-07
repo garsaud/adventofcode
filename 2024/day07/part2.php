29bc32b48ca0e042af50b7e66abf673b40311800
garsaud <cyril.garsaud@jurihub.fr>
2024-12-07 18:06:41 +0100

    2024: day7 part2

--- 2024/day07/part2.php
+++ 2024/day07/part2.php
@@ -8,12 +8,16 @@ $instructions = array_map(fn($line) => sscanf($line, '%d: %[^[]]'), $lines);
 $instructions_that_are_true = array_filter($instructions, function ($ins) {
     $parts = explode(' ', $ins[1]);
     $spaces = count($parts) - 1;
-    $max = pow(2, $spaces);
+    $max = pow(3, $spaces);
     for ($permutation = 0; $permutation < $max; $permutation++) {
-        $operations = str_split(sprintf("%0{$spaces}b", $permutation));
+        $operations = array_pad(str_split(base_convert($permutation, 10, 3)), -$spaces, 0);
         $result = array_reduce(
             array_keys($operations),
-            fn($acc, $i) => $operations[$i] ? $acc + $parts[$i+1] : $acc * $parts[$i+1],
+            fn($acc, $i) => match ((int)$operations[$i]) {
+                0 => $acc * $parts[$i+1],
+                1 => $acc + $parts[$i+1],
+                2 => $acc . $parts[$i+1],
+            },
             $parts[0]
         );
         if ($result == $ins[0]) return true;
