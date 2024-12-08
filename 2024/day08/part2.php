92e786a7e1cbdf74d88aa39c1e159f0434dc6eaa
garsaud <cyril.garsaud@jurihub.fr>
2024-12-08 12:31:30 +0100

    2024: day8 part2

--- 2024/day08/part2.php
+++ 2024/day08/part2.php
@@ -12,7 +12,12 @@ $find_antennas_alike = function ($y, $x) use ($matrix) {
             if ($cell !== $freq) continue; // skip other freqs
             if ($y0 == $y && $x0 == $x) continue; // ignore self
             $vector = [$y0-$y,$x0-$x];
-            $finds[] = [$y0+$vector[0], $x0+$vector[1]];
+            $antinode = [$y0, $x0];
+            for (;;) {
+                $finds[] = $antinode;
+                $antinode = [$antinode[0]+$vector[0], $antinode[1]+$vector[1]];
+                if (!isset($matrix[$antinode[0]][$antinode[1]])) break; // map border reached
+            }
         }
     }
     return $finds;
@@ -24,7 +29,6 @@ foreach ($matrix as $y => $row) {
         if ($cell === '.') continue; // skip empty locations
         $others = $find_antennas_alike($y, $x);
         foreach ($others as $other) {
-            if (!isset($matrix[$other[0]][$other[1]])) continue; // skip off-map antinodes
             $unique_antinodes["{$other[0]},{$other[1]}"] = null;
         }
     }
